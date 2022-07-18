6175da2b2850d288028a46df19b543bc0095d7a7
theDTS1 <orel1one@gmail.com>
2022-07-18 15:29:47 +0300

    First draft

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # python-news-desktop-app
 A simple desktop app for news in Bulgarian and English. 
 
-## Powered by PyQt5
+## Powered by tkinter
+
+This is a simple application, that provides top 5 BG and EN news via NewsDataAPI. 
+
+You will need to provide you own API KEY! 
---
+++ main.py
@@ -0,0 +1,36 @@
+from newsdataapi import NewsDataApiClient
+from tkinter import *
+
+from tkinter import scrolledtext
+
+
+# API key authorization, Initialize the client with your API key
+
+api = NewsDataApiClient(apikey="NewsDataAPI")
+
+# You can pass empty or with request parameters {ex. (country = "us")}
+
+response = api.news_api(
+    language="bg, en", category="business, technology, top, entertainment")
+
+data = response["results"]
+
+window = Tk()
+
+window.title("Welcome to LikeGeeks app")
+
+window.geometry('800x600')
+
+txt = scrolledtext.ScrolledText(window, width=50, height=60)
+
+txt.insert(INSERT, data[0]["title"] + "\n" + data[0]["description"] + "\n" + data[0]["link"]+"\n"+"\n"+data[1]["title"] + "\n" +
+           data[1]["description"] + "\n" + data[1]["link"]+"\n"+"\n"+data[2]["title"] + "\n" + data[2]["description"] + "\n" + data[2]["link"]+"\n"+"\n"+data[3]["title"] + "\n" + data[3]["description"] + "\n" + data[3]["link"]+"\n"+"\n"+data[4]["title"] + "\n" + data[4]["description"] + "\n" + data[4]["link"])
+
+txt.grid(column=0, row=0)
+
+exit_button = Button(window, text="Exit", command=window.destroy)
+exit_button.grid(column=0, row=1)
+
+window.attributes('-fullscreen', True)
+
+window.mainloop()
